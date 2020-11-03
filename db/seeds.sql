1371afbebe0c62dc1bc4c5a75b0d5a857bb121d8
CJ Dodge <hitmancjack@yahoo.com>
2020-11-03 14:39:55 -0800

    created db folder and added schema.sql file for burgers and added seeds.sql for burger types

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Avacado Bacon Burger');
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
+INSERT INTO burgers (burger_name) VALUES ('Chili Burger');
